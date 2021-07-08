d093624731a9f7c37592e6cea820c21555c024cf
Dylan Mejia <86011694+Extremite787@users.noreply.github.com>
2021-07-07 21:50:03 -0500

    modific botones

--- Calculadora/Form1.cs
+++ Calculadora/Form1.cs
@@ -22,7 +22,7 @@ namespace Calculadora
         }
         private void butnumero0_Click(object sender, EventArgs e)
         {
-            Numero = Numero + "0";
+            Pantallapri.Text = Pantallapri.Text+"0";
         }
 
         private void Pantallapri_TextChanged(object sender, EventArgs e)
@@ -32,47 +32,47 @@ namespace Calculadora
 
         private void butnumero1_Click(object sender, EventArgs e)
         {
-            Numero = Numero + "1";
+            Pantallapri.Text = Pantallapri.Text + "1";
         }
 
         private void butnumero2_Click(object sender, EventArgs e)
         {
-            Numero = Numero + "2";
+            Pantallapri.Text = Pantallapri.Text + "2";
         }
 
         private void butnumero3_Click(object sender, EventArgs e)
         {
-            Numero = Numero + "3";
+            Pantallapri.Text = Pantallapri.Text + "3";
         }
 
         private void butnumero4_Click(object sender, EventArgs e)
         {
-            Numero = Numero + "4";
+            Pantallapri.Text = Pantallapri.Text + "4";
         }
 
         private void butnumero5_Click(object sender, EventArgs e)
         {
-            Numero = Numero + "5";
+            Pantallapri.Text = Pantallapri.Text + "5";
         }
 
         private void butnumero6_Click(object sender, EventArgs e)
         {
-            Numero = Numero + "6";
+            Pantallapri.Text = Pantallapri.Text + "6";
         }
 
         private void butnumero7_Click(object sender, EventArgs e)
         {
-            Numero = Numero + "7";
+            Pantallapri.Text = Pantallapri.Text + "7";
         }
 
         private void butnumero8_Click(object sender, EventArgs e)
         {
-            Numero = Numero + "8";
+            Pantallapri.Text = Pantallapri.Text + "8";
         }
 
         private void butnumero9_Click(object sender, EventArgs e)
         {
-            Numero = Numero + "9";
+            Pantallapri.Text = Pantallapri.Text + "9";
         }
 
         private void butsuma_Click(object sender, EventArgs e)
